fd229a7231be52b28cb24c6d59f73dfdd4be632b
JerryWSL <jerryshum2636@gmail.com>
2026-02-16 12:24:20 -0500

    define types to be used later.

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+import type { Edge, Node } from '@xyflow/react';
+
+//! Defining out custom types for each custom node --> we are defining each Node's data points
+// - The colornode "data" field will only have a "color"
+// - The TextNode "data" field will only have a "label"
+type ColorNodeData = {
+   color: string;
+   type: 'color';
+};
+
+type TextNodeData = {
+   label: string;
+   type: 'text';
+};
+
+//! We are creating the Union Type "AppNodeData" --> we pass into the ReactFlow's Node type as a generic
+type AppNodeData = ColorNodeData | TextNodeData;
+
+//! Reactflow provides this BASE node type --> contains generic properties like id, position, type
+//@ Has a generic (placeholder) for the data property (since it can contain many different forms)
+// Using the NODE type as a base --> we can build upon it by passing in APPNODEDATA to fill in the generic --> creating a special node that only uses our specific AppNodeData
+export type AppNode = Node<AppNodeData>;
+
+//---------------------------
+
+//# Interface for the zustand store
+// creating an interface for our zustand store --> these are the properties / states that it MUST contain
+// nodes --> array of app nodes
+// edges --> array of edges
+export interface NodeStore {
+   nodes: AppNode[];
+   edges: Edge[];
+}
